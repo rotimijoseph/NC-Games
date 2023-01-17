3c5446b9e5b21fb29e027510eaf8b83944fe1299
Rotimi Joseph <Rotimi@Rotimis-MacBook-Pro.local>
2023-01-17 14:34:17 +0000

    more changes

--- app/app.js
+++ app/app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.get('/api/categories', getCategories);
 app.get('/api/reviews', getReviews);
 
-app.get()
+// app.get()
 
 app.use((request, response, next) => {
     response.status(404).send({ msg: "Path not found"})
