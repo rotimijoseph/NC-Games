eda79b15c5d93b2e0fb25c0b57e127e4eda831f3
Rotimi Joseph <Rotimi@Rotimis-MacBook-Pro.local>
2023-01-17 12:11:44 +0000

    add task 4

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,7 @@ const request = require("supertest");
 const data = require("../db/data/test-data/index");
 const db = require("../db/connection");
 const seed = require("../db/seeds/seed");
+require("jest-sorted");
 
 beforeEach(() => {
     return seed(data);
@@ -11,6 +12,7 @@ beforeEach(() => {
   afterAll(() => {
     db.end();
   });
+
 
 describe("app", () => {
     describe("GET/api/categories", () => {
@@ -50,4 +52,59 @@ describe("app", () => {
             })
         })
         })
+    describe.only("GET/api/reviews", () => {
+	    test("200 status code: request has been succeeded", () => {
+		    return request(app)
+		    .get("/api/reviews")
+		    .expect(200)
+		    })
+        test("200 status code: should resolve with an array of objects", () => {
+            return request(app)
+            .get("/api/reviews")
+            .expect(200)
+            .then((result) => {
+                const reviews = result.body
+                expect(reviews).toBeInstanceOf(Array)
+            })
+        })
+        test("200 status code: array of objects created with correct keys", () => {
+            return request(app)
+            .get("/api/reviews")
+            .expect(200)
+            .then((result) => {
+                const reviewsResult = result.body
+                reviewsResult.forEach((review) => {
+                    expect(review).toHaveProperty('owner', expect.any(String)); 
+                    expect(review).toHaveProperty('title', expect.any(String)); 
+                    expect(review).toHaveProperty('designer', expect.any(String)); 
+                    expect(review).toHaveProperty('review_id', expect.any(Number)); 
+                    expect(review).toHaveProperty('category', expect.any(String)); 
+                    expect(review).toHaveProperty('review_img_url', expect.any(String)); 
+                    expect(review).toHaveProperty('created_at', expect.any(String)); 
+                    expect(review).toHaveProperty('votes', expect.any(Number)); 
+                    expect(review).toHaveProperty('comment_count'); 
+                })
+            })
+        })
+        test("200 status code: reviews are sorted by date in descending order", () => {
+            return request(app)
+            .get("/api/reviews")
+            .expect(200)
+            .then((result) => {
+                const reviewsResult = result.body
+                expect(reviewsResult).toBeSortedBy("created_at", {descending: true})
+            })
+        })
+        test("404 status code: path was not found", () => {
+            return request(app)
+            .get("/wrong-path")
+            .expect(404)
+            .then((result) => {
+                const response = result.body
+                expect(response.msg).toBe("Path not found")
+            })
+        })
+        })
+
     });
+
--- models/model.js
+++ models/model.js
@@ -5,5 +5,15 @@ const fetchCategories = () => {
     .then((result) => result.rows)
 }
 
+const fetchReviews = () => {
+    let queryStr = `SELECT owner, title, designer, reviews.review_id, category, review_img_url, reviews.created_at, reviews.votes,
+    COUNT(comments.review_id) AS comment_count 
+    FROM reviews
+    LEFT JOIN comments ON comments.review_id = reviews.review_id
+    GROUP BY reviews.review_id
+    ORDER BY created_at DESC;`
+    return db.query(queryStr)
+    .then((result) => result.rows)  
+}
 
-module.exports = { fetchCategories } 
+module.exports = { fetchCategories, fetchReviews } 
